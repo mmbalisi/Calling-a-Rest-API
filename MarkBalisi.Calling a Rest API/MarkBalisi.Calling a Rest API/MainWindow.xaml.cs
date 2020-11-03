a9b363a0457c33e73c26d991fa61ec02e63f3e31
DESKTOP-0UPI14J\hp <mmbalisi@csmbataan.ph.education>
2020-11-03 19:00:16 +0800

    Adding Working Code/Finish Project

---
+++ MarkBalisi.Calling a Rest API/MarkBalisi.Calling a Rest API/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarkBalisi.Calling_a_Rest_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowPhotos();
+        }
+        private void ShowPhotos()
+        {
+            var Client = new RestClient("https://jsonplaceholder.typicode.com/photos/38");
+
+            var request = new RestRequest("", Method.GET);
+
+            IRestResponse response = Client.Execute(request);
+
+            var content = response.Content;
+
+            var data = JsonConvert.DeserializeObject<photos>(content);
+            lblAlbumId.Content = "AlbumId: " + data.albumId;
+            lblId.Content = "Id: " + data.id;
+            lblTitle.Content = "Title: " + data.title;
+            lblUrl.Content = "Url: " + data.url;
+            lblThumbnailUrl.Content = "ThumbnailUrl: " + data.thumbnailUrl;
+        }
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPhotos();
+        }
+    }
+}
---
+++ MarkBalisi.Calling a Rest API/MarkBalisi.Calling a Rest API/photos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkBalisi.Calling_a_Rest_API
+{
+    public class photos
+    {
+        public string albumId { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string thumbnailUrl { get; set; }
+    }
+}
